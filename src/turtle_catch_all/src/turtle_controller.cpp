201740eb3855e5ccca7d3c9e8d0d16a608735022
Artur Bertash <arturbertash88@gmail.com>
2026-02-22 16:54:25 -0700

    add lacun file

--- src/turtle_catch_all/src/turtle_controller.cpp
+++ src/turtle_catch_all/src/turtle_controller.cpp
@@ -1,17 +1,100 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-
-
+#include "my_turtle_interfaces/msg/list_turtles.hpp"
+#include "my_turtle_interfaces/msg/turtle.hpp"
+#include "turtlesim/msg/pose.hpp"
+#include "turtlesim/srv/kill.hpp"
+#include <cmath>
+#include <string>
 
 class TurtleControllerNode : public rclcpp::Node
 {
     public:
         TurtleControllerNode() : Node("turtle_controller") {
-             publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+            publisher_pose_ = this->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+            subscriber_turtles_ = this->create_subscription<my_turtle_interfaces::msg::ListTurtles>("/turtle_list", 10, std::bind(&TurtleControllerNode::turtle_list_callback, this, std::placeholders::_1));
+            timer_ = this->create_wall_timer(std::chrono::milliseconds(200), std::bind(&TurtleControllerNode::control_loop, this));
+            subscriber_pose_ = this->create_subscription<turtlesim::msg::Pose>("/turtle1/pose", 10, std::bind(&TurtleControllerNode::update_pose_callback, this, std::placeholders::_1));
+            client_kill_ = this->create_client<turtlesim::srv::Kill>("/my_kill_turtle");
         }
 
     private:
-        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_pose_;
+        rclcpp::Subscription<my_turtle_interfaces::msg::ListTurtles>::SharedPtr subscriber_turtles_;
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber_pose_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Client<turtlesim::srv::Kill>::SharedPtr client_kill_;
+
+        my_turtle_interfaces::msg::Turtle target_turtle_;
+        bool has_target_ = false;
+
+        turtlesim::msg::Pose current_pose_;
+        bool pose_received_ = false;
+
+        void turtle_list_callback(const my_turtle_interfaces::msg::ListTurtles::SharedPtr list_turtles) {
+            if (list_turtles->turtles.empty()) {
+                RCLCPP_WARN(this->get_logger(), "No turtles spawned yet");
+            }
+
+            if (!has_target_) {
+                target_turtle_ = list_turtles->turtles[0];
+                has_target_ = true;
+            }
+        }
+
+        void control_loop() {
+            if (!has_target_) {
+                return;
+            }
+
+            double dx = target_turtle_.x - current_pose_.x;
+            double dy = target_turtle_.y - current_pose_.y;
+
+            double distance = std::sqrt(dx * dx + dy * dy);
+            
+            double desired_angle = std::atan2(dy, dx);
+            double error_angle = desired_angle - current_pose_.theta;
+
+            while (error_angle > M_PI) {
+                error_angle -= 2.0 * M_PI;
+            }
+            while (error_angle < -M_PI)
+                error_angle+= 2.0 * M_PI;
+
+            geometry_msgs::msg::Twist cmd;
+            double k_gains_linear = 2.0;
+            double k_gain_angular = 4.0;
+
+            if (distance < 0.1) {
+                cmd.linear.x = 0;
+                cmd.angular.z = 0;
+                has_target_ = false;  
+
+                auto kill_name_turtle_msg = std::make_shared<turtlesim::srv::Kill::Request>(); 
+                kill_name_turtle_msg->name = target_turtle_.name;
+                client_kill_->async_send_request(kill_name_turtle_msg);
+
+                RCLCPP_INFO(this->get_logger(), "Target reached!");
+                
+
+
+            } else {
+                cmd.linear.x = distance * k_gains_linear;
+                cmd.angular.z = error_angle * k_gain_angular;
+            }
+
+            publisher_pose_->publish(cmd);
+
+            
+        }   
+
+        void update_pose_callback(turtlesim::msg::Pose::SharedPtr new_pose) {
+            current_pose_ = *new_pose;
+            pose_received_ = true;
+        }
+
+        
+
 };
 
 
--- src/turtle_catch_all/src/turtle_spawner.cpp
+++ src/turtle_catch_all/src/turtle_spawner.cpp
@@ -2,7 +2,8 @@
 #include "turtlesim/srv/spawn.hpp"
 #include "my_turtle_interfaces/msg/list_turtles.hpp"
 #include "my_turtle_interfaces/msg/turtle.hpp"
-
+#include "turtlesim/srv/kill.hpp"
+#include <algorithm>
 #include <random>
 
 
@@ -14,6 +15,8 @@ class TurtleSpawnerNode : public rclcpp::Node
             counter_ = 2;
             timer_ = this->create_wall_timer(std::chrono::milliseconds(2000), std::bind(&TurtleSpawnerNode::timer_callback, this));
             publisher_ = this->create_publisher<my_turtle_interfaces::msg::ListTurtles>("turtle_list", 10);
+            server_ = this->create_service<turtlesim::srv::Kill>("/my_kill_turtle", std::bind(&TurtleSpawnerNode::kill_turtle_server_callback, this, std::placeholders::_1, std::placeholders::_2));
+            client_kill_ = this->create_client<turtlesim::srv::Kill>("/kill");
         }
         
         
@@ -24,12 +27,13 @@ class TurtleSpawnerNode : public rclcpp::Node
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<my_turtle_interfaces::msg::ListTurtles>::SharedPtr publisher_;
         std::vector<my_turtle_interfaces::msg::Turtle> vector_turtles;
-
+        rclcpp::Service<turtlesim::srv::Kill>::SharedPtr server_;
+        rclcpp::Client<turtlesim::srv::Kill>::SharedPtr client_kill_;
     
 
         int counter_;
         void client_callback(rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture future, double x, double y, std::string name) {
-            auto response = future.get();
+            (void)future;
             auto turtle = my_turtle_interfaces::msg::Turtle();
             turtle.name = name;
             turtle.x = x;
@@ -39,12 +43,12 @@ class TurtleSpawnerNode : public rclcpp::Node
             auto list_turtles = my_turtle_interfaces::msg::ListTurtles();
             list_turtles.turtles = vector_turtles;
             publisher_->publish(list_turtles);
-            
+
             RCLCPP_INFO(this->get_logger(), "Turtle %s spawned at (%.2f, %.2f)", name.c_str(), x, y);
         }
 
         void spawn_turtle() {
-            while (!client_->wait_for_service(std::chrono::milliseconds(2000))) {
+            while (!client_->wait_for_service(std::chrono::milliseconds(1000))) {
                 RCLCPP_WARN(this->get_logger(), "Waiting for /spawn server");
             }
             auto random_turtle = std::make_shared<turtlesim::srv::Spawn::Request>();
@@ -69,6 +73,46 @@ class TurtleSpawnerNode : public rclcpp::Node
         }
         void timer_callback() {
             this->spawn_turtle();
+        }
+
+
+        void kill_turtle_server_callback(const turtlesim::srv::Kill::Request::SharedPtr req, 
+                                   turtlesim::srv::Kill::Response::SharedPtr res ) {
+            (void)res;
+            while (!client_kill_->wait_for_service(std::chrono::milliseconds(2000))) {
+                RCLCPP_WARN(this->get_logger(), "Waiting for /Kill server");
+            }
+
+            auto kill_name_msg = std::make_shared<turtlesim::srv::Kill::Request>();
+            kill_name_msg->name = req->name;
+
+            
+            auto future = client_kill_->async_send_request(kill_name_msg, 
+                [this, name = req->name](rclcpp::Client<turtlesim::srv::Kill>::SharedFuture future) {
+                    service_kill_callback(future, name);
+                });
+            
+            
+
+        }
+
+        void service_kill_callback(rclcpp::Client<turtlesim::srv::Kill>::SharedFuture future, std::string name) {
+            (void)future;
+            vector_turtles.erase(
+                std::remove_if(
+                    vector_turtles.begin(),
+                    vector_turtles.end(),
+                    [&](const my_turtle_interfaces::msg::Turtle & t) {
+                        return t.name == name;
+                    }),
+                vector_turtles.end()
+            );
+
+            RCLCPP_INFO(this->get_logger(), "The turtle is killed");
+
+            auto list_msg = my_turtle_interfaces::msg::ListTurtles();
+            list_msg.turtles = vector_turtles;
+            publisher_->publish(list_msg);
         }
     
 };
